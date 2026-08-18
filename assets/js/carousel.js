fb76e93382ec7c15179fb8406dca326b199c1a7d
Aakash Chavan <97724881+ChavanAakash@users.noreply.github.com>
2026-08-18 16:10:35 +0530

    new pages added

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -69,3 +69,76 @@ $(document).ready(function () {
         }
     })
 })
+
+
+$(document).ready(function () {
+
+    $('.client-logo-carousel').owlCarousel({
+
+        loop: true,
+
+        margin: 10,
+
+        nav: true,
+
+        dots: false,
+
+        autoplay: true,
+
+        autoplayTimeout: 2000,
+
+        autoplayHoverPause: true,
+
+        smartSpeed: 600,
+
+        responsive: {
+
+            0: {
+                items: 1
+            },
+
+            576: {
+                items: 2
+            },
+
+            768: {
+                items: 3
+            },
+
+            992: {
+                items: 4
+            }
+
+        }
+
+    });
+
+
+    // Client Logo Popup
+    $('.client-popup').on('click', function (e) {
+
+        e.preventDefault();
+
+        var imageUrl = $(this).attr('href');
+
+        $('body').append(
+            '<div class="client-image-popup">' +
+                '<div class="client-popup-overlay"></div>' +
+                '<div class="client-popup-content">' +
+                    '<button class="client-popup-close">&times;</button>' +
+                    '<img src="' + imageUrl + '" alt="Client Logo">' +
+                '</div>' +
+            '</div>'
+        );
+
+    });
+
+
+    // Close popup
+    $(document).on('click', '.client-popup-close, .client-popup-overlay', function () {
+
+        $('.client-image-popup').remove();
+
+    });
+
+});
